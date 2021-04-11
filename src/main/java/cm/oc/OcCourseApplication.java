ef1697c2a1567b7b4027d0d655573a532625724d
LeKuetebi <nkuetebi@yahoo.fr>
2021-04-11 13:49:17 +0100

    Initialisation de mon dépot

---
+++ src/main/java/cm/oc/OcCourseApplication.java
@@ -0,0 +1,13 @@
+package cm.oc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cm/oc/OcCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.oc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
