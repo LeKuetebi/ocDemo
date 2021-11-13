19e6a890dff5677f96ca30b86fc80e3adb253426
LeKuetebi <nkuetebi@yahoo.fr>
2021-11-13 09:11:41 -0500

    Update OcCourseApplication.java

--- src/main/java/cm/oc/OcCourseApplication.java
+++ src/main/java/cm/oc/OcCourseApplication.java
@@ -8,8 +8,6 @@ public class OcCourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OcCourseApplication.class, args);
-		
-		System.out.println("Program launched");
 	}
 
 }
